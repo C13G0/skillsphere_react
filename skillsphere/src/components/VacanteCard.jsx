cd5f4b9b87c95a7e714cd6a2b43a8ea2bc0f8dad
lusuga404 <lusuga404@gmail.com>
2026-04-22 21:39:09 -0500

    feat: formulario agregar vacantes y boton aplicar

--- skillsphere/src/components/VacanteCard.jsx
+++ skillsphere/src/components/VacanteCard.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import './VacanteCard.css'
 
 function VacanteCard(props) {
+  const [aplicado, setAplicado] = useState(false)
+
   return (
     <div className="vacante-card">
       <h2>{props.nombre}</h2>
@@ -8,8 +11,10 @@ function VacanteCard(props) {
       <p className="empresa">{props.empresa}</p>
       <p className="salario">{props.salario}</p>
       <p className="disponibilidad">{props.disponibilidad}</p>
-      <button>Aplicar</button>
-
+      <button onClick={() => setAplicado(true)} disabled={aplicado}>
+        {aplicado ? "Aplicado" : "Aplicar"}
+      </button>
+      <button onClick={props.onEliminar}>Eliminar</button>
     </div>
   )
 }
